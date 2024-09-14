61dc6d08e27ae2daffe452552e95eb3a3e90b36d
duddlshin <duddlshin@gmail.com>
2024-09-13 21:33:46 -0700

    Update on half channel concurrent files from Yellowstone

--- problems/incompressible/half_channel_concurrent_files/padeops_runs_template/template.sh
+++ problems/incompressible/half_channel_concurrent_files/padeops_runs_template/template.sh
@@ -2,15 +2,14 @@
 #SBATCH --job-name=wide
 #SBATCH -N 2
 #SBATCH -n 64
-#SBATCH --time=00:59:00
+#SBATCH --time=00:30:00
 #SBATCH --partition=cpu
-#SBATCH --exclude=yellowstone-cpu-3-1,yellowstone-cpu-11-20
 #SBATCH --mail-type=BEGIN,END            # Mail when job starts and ends
 #SBATCH --mail-user=youngin@mit.edu      # Email recipient
 
 
 export inputFile="input_concurrent.dat"
-export problemDir="/home/ctrsp-2024/youngin/mypadeops/PadeOps/problems/incompressible/half_channel_concurrent_files/padeops_runs"
+export problemDir="/home/ctrsp-2024/youngin/mypadeops/PadeOps/problems/incompressible/half_channel_concurrent_files/padeops_runs_template"
 
 cd /home/ctrsp-2024/youngin/mypadeops/PadeOps/build_opti/problems/incompressible
 date
